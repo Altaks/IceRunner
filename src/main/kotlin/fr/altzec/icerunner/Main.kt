5d5c548b7f8c378204a7bdda003a1199a433f136
Altaks <altair61.dev@gmail.com>
2025-10-14 21:55:35 +0200

    feat: added a basic game manager, a debug command and a listener that triggers the start of the game when enough players are on the server

--- src/main/kotlin/fr/altzec/icerunner/Main.kt
+++ src/main/kotlin/fr/altzec/icerunner/Main.kt
@@ -1,22 +1,40 @@
 package fr.altzec.fr.altzec.icerunner
 
+import fr.altzec.fr.altzec.icerunner.game.GameManager
+import fr.altzec.fr.altzec.icerunner.triggers.commands.DevCommand
 import fr.altzec.fr.altzec.icerunner.triggers.listeners.PlayerJoinQuitListener
-import lombok.Getter
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.PluginCommand
+import org.bukkit.command.TabExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
-    @Getter
-    private val pluginLogger = Bukkit.getLogger()
+    companion object {
+        val MAIN_PREFIX: String = "${ChatColor.GRAY}[${ChatColor.BLUE}IceRunner${ChatColor.GRAY}] \u00BB${ChatColor.RESET}";
+    }
+
+    val gameManager: GameManager = GameManager(this);
+    val pluginLogger = Bukkit.getLogger()
 
     override fun onEnable() {
         super.onEnable()
 
-        Bukkit.getPluginManager().registerEvents(PlayerJoinQuitListener(), this)
+        registerCommand("dev", DevCommand(this));
+
+        Bukkit.getPluginManager().registerEvents(PlayerJoinQuitListener(this), this);
     }
 
     override fun onDisable() {
         super.onDisable()
+    }
+
+    fun registerCommand(command: String, tabExecutor: TabExecutor) {
+        val command: PluginCommand? = getCommand(command);
+        command?.setExecutor(tabExecutor);
+        command?.tabCompleter = tabExecutor;
+        pluginLogger.info("Command ${command?.usage} has been registered");
     }
 }
