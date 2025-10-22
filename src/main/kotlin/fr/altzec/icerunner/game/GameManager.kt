565b2f70e3b41c21b580aded0e3172a85ba57601
Altaks <altair61.dev@gmail.com>
2025-10-22 15:51:38 +0200

    feat: added proper player respawn, jump boost, hunger resistance, nofall and disabled item drops

--- src/main/kotlin/fr/altzec/icerunner/game/GameManager.kt
+++ src/main/kotlin/fr/altzec/icerunner/game/GameManager.kt
@@ -7,20 +7,41 @@ import fr.altzec.fr.altzec.icerunner.triggers.tasks.StartingPhaseTask
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.GameMode
+import org.bukkit.damage.DamageSource
+import org.bukkit.damage.DamageType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scheduler.BukkitTask
 
-class GameManager(val main: Main) {
+class GameManager(val main: Main) : Listener {
+
+    companion object {
+        private const val NO_TASK_DELAY = 0L;
+        private const val EVERY_TICK = 1L;
+        private const val EVERY_SECOND = EVERY_TICK * 20L;
+
+        private const val INFINITE_POTION_EFFECT_DURATION = 1_000_000;
+        private const val JUMP_BOOST_AMPLIFIER = 1; // 2 blocks high
+
+        private val CONGRATS_DECORATION = "${ChatColor.RED}${ChatColor.MAGIC}!${ChatColor.AQUA}${ChatColor.MAGIC}!${ChatColor.GREEN}${ChatColor.MAGIC}!${ChatColor.LIGHT_PURPLE}${ChatColor.MAGIC}!${ChatColor.GOLD}${ChatColor.MAGIC}!"
+        private val CONGRATS_DECORATION_REVERSED = "${ChatColor.GOLD}${ChatColor.MAGIC}!${ChatColor.LIGHT_PURPLE}${ChatColor.MAGIC}!${ChatColor.GREEN}${ChatColor.MAGIC}!${ChatColor.AQUA}${ChatColor.MAGIC}!${ChatColor.RED}${ChatColor.MAGIC}!"
+    }
 
     private var gameState: GameState = GameState.WAITING
 
     private var startingPhaseTask: BukkitTask? = null
+    private var bifrostTask: BukkitTask? = null
     private var playingPhaseTask: BukkitTask? = null
 
-    companion object {
-        private val CONGRATS_DECORATION = "${ChatColor.RED}${ChatColor.MAGIC}!${ChatColor.AQUA}${ChatColor.MAGIC}!${ChatColor.GREEN}${ChatColor.MAGIC}!${ChatColor.LIGHT_PURPLE}${ChatColor.MAGIC}!${ChatColor.GOLD}${ChatColor.MAGIC}!"
-        private val CONGRATS_DECORATION_REVERSED = "${ChatColor.GOLD}${ChatColor.MAGIC}!${ChatColor.LIGHT_PURPLE}${ChatColor.MAGIC}!${ChatColor.GREEN}${ChatColor.MAGIC}!${ChatColor.AQUA}${ChatColor.MAGIC}!${ChatColor.RED}${ChatColor.MAGIC}!"
-    }
 
     fun triggerStartingGamePhase() {
         this.gameState = GameState.STARTING
@@ -30,10 +51,11 @@ class GameManager(val main: Main) {
         this.main.worldManager.teleportPlayersToGameWorld()
 
         // Starting and storing the startingPhaseTask
-        this.startingPhaseTask = StartingPhaseTask(this.main).runTaskTimer(this.main, 0, 20)
+        this.startingPhaseTask = StartingPhaseTask(this.main).runTaskTimer(this.main, NO_TASK_DELAY, EVERY_SECOND)
     }
 
     fun triggerPlayingGamePhase() {
+        this.gameState = GameState.PLAYING
         this.gameState = GameState.PLAYING
         Bukkit.broadcastMessage(
             "${Main.MAIN_PREFIX}${ChatColor.LIGHT_PURPLE} La partie commence !...\n" +
@@ -56,9 +78,11 @@ class GameManager(val main: Main) {
             player.level = 0
             player.foodLevel = 20
             player.health = 20.0
+            player.addPotionEffect(PotionEffect(PotionEffectType.JUMP_BOOST, INFINITE_POTION_EFFECT_DURATION, JUMP_BOOST_AMPLIFIER))
         }
-        BifrostTask().runTaskTimer(this.main, 0,1)
 
+        // Just for testing purposes
+        this.bifrostTask = BifrostTask().runTaskTimer(this.main, NO_TASK_DELAY,EVERY_TICK)
         this.playingPhaseTask = PlayingPhaseTask(this.main).runTaskTimer(this.main, 0, 20)
     }
 
@@ -79,5 +103,54 @@ class GameManager(val main: Main) {
             player.health = 20.0
             player.inventory.clear()
         }
+    }
+
+    @EventHandler
+    fun onPlayerFallsIntoTheVoid(event: EntityDamageEvent) {
+        if(event.cause != EntityDamageEvent.DamageCause.VOID) return;
+        if(event.entity is Player) {
+            val player = event.entity as Player;
+
+            when (this.gameState) {
+                GameState.PLAYING -> {
+                    // Cancel the event first
+                    event.isCancelled = true
+
+                    val playerTeam = this.main.teamsManager.getPlayerGameTeam(player)
+
+                    // Teleport them to their base
+                    val respawnPoint = playerTeam.respawnPoint(this.main.worldManager.loadedWorldMetadata ?: throw IllegalStateException("The loaded world variant metadata should exist"));
+                    player.teleport(respawnPoint)
+
+                    player.exp = 0.0F
+                    player.level = 0
+
+                    player.activePotionEffects.forEach { effect -> player.removePotionEffect(effect.type) }
+                    player.addPotionEffect(PotionEffect(PotionEffectType.JUMP_BOOST, INFINITE_POTION_EFFECT_DURATION, JUMP_BOOST_AMPLIFIER))
+
+                    // Reapply inventory to player
+                    GameItems.applyPlayingInventoryToPlayer(player, playerTeam.armorColor)
+                }
+                else -> {}
+            }
+        }
+    }
+
+    @EventHandler
+    fun onPlayerHungerChanges(event: FoodLevelChangeEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onEntityTakesFallDamage(event: EntityDamageEvent) {
+        if (event.cause == EntityDamageEvent.DamageCause.FALL) {
+            event.isCancelled = true;
+        }
+    }
+
+    @EventHandler
+    fun onPlayerDropsItem(event: PlayerDropItemEvent) {
+        // No matter the game state, no one should be able to drop items
+        event.isCancelled = true;
     }
 }
