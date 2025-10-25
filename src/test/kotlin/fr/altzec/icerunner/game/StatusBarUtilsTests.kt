a0e30e595f18f9fe86f22ab56ce15a9ca2c93881
Altaks <altair61.dev@gmail.com>
2025-10-25 11:26:56 +0200

    feat: status bar generation + tests

---
+++ src/test/kotlin/fr/altzec/icerunner/game/StatusBarUtilsTests.kt
@@ -0,0 +1,76 @@
+@file:Suppress("DEPRECATION")
+
+package fr.altzec.icerunner.game
+
+import fr.altzec.fr.altzec.icerunner.utils.StatusBarUtils
+import org.bukkit.ChatColor
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+open class StatusBarUtilsTests {
+
+    @Test
+    fun testGenerateBasicStatusBar() {
+        val barState = StatusBarUtils.StatusBarState(7, 7, 4);
+        val expectedStatusBar = "${ChatColor.GREEN}■■■■${ChatColor.GRAY}□□□";
+
+        val statusBar = StatusBarUtils.buildProgressBar(barState);
+        assert(statusBar == expectedStatusBar)
+    }
+
+    @Test
+    fun testGenerateRTLBasicStatusBar() {
+        val barState = StatusBarUtils.StatusBarState(2, 100, 50);
+        val expectedStatusBar = "${ChatColor.GRAY}□${ChatColor.GREEN}■";
+
+        val statusBar = StatusBarUtils.buildProgressBar(barState, StatusBarUtils.StatusBarStyle(leftToRight = false));
+        assert(statusBar == expectedStatusBar)
+    }
+
+    @Test
+    fun testGenerateNonDefaultSymbolStatusBar() {
+        val barStyle = StatusBarUtils.StatusBarStyle(activeSymbol = ")", baseSymbol = "!");
+        val barState = StatusBarUtils.StatusBarState(2, 100, 50);
+        val expectedStatusBar = "${ChatColor.GREEN})${ChatColor.GRAY}!";
+
+        val statusBar = StatusBarUtils.buildProgressBar(barState, barStyle);
+        assert(statusBar == expectedStatusBar)
+    }
+
+    @Test
+    fun testGenerateNonDefaultColorsStatusBar() {
+        val barStyle = StatusBarUtils.StatusBarStyle(activeColor = ChatColor.AQUA, baseColor = ChatColor.GOLD);
+        val barState = StatusBarUtils.StatusBarState(2, 100, 50);
+        val expectedStatusBar = "${ChatColor.AQUA}■${ChatColor.GOLD}□";
+
+        val statusBar = StatusBarUtils.buildProgressBar(barState, barStyle);
+        assert(statusBar == expectedStatusBar)
+    }
+
+    @Test
+    fun testGenerateWaitingPhaseRedTeamStatusBar() {
+        val teamSize = 7;
+        val teamCurrentPlayersAmount = Random.nextInt(0, 7);
+
+        val barStyle = StatusBarUtils.StatusBarStyle(baseSymbol = "◆", activeSymbol = "◆", activeColor = ChatColor.RED)
+        val barState = StatusBarUtils.StatusBarState(teamSize, teamSize, teamCurrentPlayersAmount);
+
+        val expectedStatusBar =  "${ChatColor.RED}${"◆".repeat(teamCurrentPlayersAmount)}${ChatColor.GRAY}${"◆".repeat(7 - teamCurrentPlayersAmount)}"
+        val statusBar = StatusBarUtils.buildProgressBar(barState, barStyle);
+        assert(statusBar == expectedStatusBar);
+    }
+
+
+    @Test
+    fun testGenerateWaitingPhaseBlueTeamStatusBar() {
+        val teamSize = 7;
+        val teamCurrentPlayersAmount = Random.nextInt(0, 7);
+
+        val barStyle = StatusBarUtils.StatusBarStyle(baseSymbol = "◆", activeSymbol = "◆", activeColor = ChatColor.AQUA)
+        val barState = StatusBarUtils.StatusBarState(teamSize, teamSize, teamCurrentPlayersAmount);
+
+        val expectedStatusBar = "${ChatColor.AQUA}${"◆".repeat(teamCurrentPlayersAmount)}${ChatColor.GRAY}${"◆".repeat(7 - teamCurrentPlayersAmount)}"
+        val statusBar = StatusBarUtils.buildProgressBar(barState, barStyle);
+        assert(statusBar == expectedStatusBar);
+    }
+}
