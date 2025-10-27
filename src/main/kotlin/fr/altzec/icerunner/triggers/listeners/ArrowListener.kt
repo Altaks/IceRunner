fc656fa409d5a0ed58a4b0251a07d910bd30f27d
Altaks <altair61.dev@gmail.com>
2025-10-27 17:16:00 +0100

    fix: arrow explosion was not a proper sphere

--- src/main/kotlin/fr/altzec/icerunner/triggers/listeners/ArrowListener.kt
+++ src/main/kotlin/fr/altzec/icerunner/triggers/listeners/ArrowListener.kt
@@ -1,31 +1,49 @@
 package fr.altzec.fr.altzec.icerunner.triggers.listeners
 
 import fr.altzec.fr.altzec.icerunner.Main
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.ProjectileHitEvent
+import org.bukkit.scheduler.BukkitScheduler
+import org.bukkit.util.NumberConversions
 
 
 class ArrowListener(val main: Main) : Listener {
+
+    companion object {
+        private const val EXPLOSION_RADIUS = 3;
+    }
+
     @EventHandler
-    fun arrowCollideBlock(event: ProjectileHitEvent){
-        if (event.entity.type == EntityType.ARROW){
-            val projectilePosition = event.hitBlock?.location ?:return
-            for (x in -3..3){
-                for(y in -3..3){
-                   for(z in -3..3){
-                        main.pluginLogger.info("block scanné =$x $y $z")
-                        val position = projectilePosition.add(x.toDouble(), y.toDouble(), z.toDouble())
-                        main.pluginLogger.info("$position")
-                        val newType = when (position.block.type) {
-                            Material.ICE -> Material.AIR
-                            Material.PACKED_ICE -> Material.ICE
-                            Material.BLUE_ICE -> Material.PACKED_ICE
-                            else -> position.block.type
+    fun onArrowCollidesBlock(event: ProjectileHitEvent) {
+        if (event.entity.type == EntityType.ARROW) {
+            if (event.hitBlock != null) {
+                event.entity.remove()
+
+                // 3d radius matrix
+                for (x in -EXPLOSION_RADIUS..EXPLOSION_RADIUS) {
+                    for (y in -EXPLOSION_RADIUS..EXPLOSION_RADIUS) {
+                        for (z in -EXPLOSION_RADIUS..EXPLOSION_RADIUS) {
+
+                            // scanned block position
+                            val position = event.hitBlock?.location?.add(x.toDouble(), y.toDouble(), z.toDouble()) ?: continue
+
+                            // if the scanned block is in the sphere of radius EXPLOSION_RADIUS
+                            if(position.distanceSquared(event.hitBlock?.location!!) <= NumberConversions.square(EXPLOSION_RADIUS.toDouble())) {
+
+                                // Apply block replacing
+                                val newType = when (position.block.type) {
+                                    Material.ICE -> Material.AIR
+                                    Material.PACKED_ICE -> Material.ICE
+                                    Material.BLUE_ICE -> Material.PACKED_ICE
+                                    else -> position.block.type
+                                }
+                                position.block.setType(newType, false)
+                            }
                         }
-                        position.block.setType(newType, false)
                     }
                 }
             }
