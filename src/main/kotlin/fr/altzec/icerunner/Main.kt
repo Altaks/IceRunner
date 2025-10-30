d3b176238f10072d7a14dc0e9bb9770df9d35cb8
Altaks <altair61.dev@gmail.com>
2025-10-30 18:09:36 +0100

    feat: added motd and server icon

--- src/main/kotlin/fr/altzec/icerunner/Main.kt
+++ src/main/kotlin/fr/altzec/icerunner/Main.kt
@@ -7,6 +7,8 @@ import fr.altzec.fr.altzec.icerunner.triggers.commands.DevCommand
 import fr.altzec.fr.altzec.icerunner.triggers.listeners.ArrowListener
 import fr.altzec.fr.altzec.icerunner.triggers.listeners.DevListener
 import fr.altzec.fr.altzec.icerunner.triggers.listeners.PlayerJoinQuitListener
+import fr.altzec.fr.altzec.icerunner.triggers.listeners.ServerListPingListener
+import fr.altzec.fr.altzec.icerunner.utils.FileUtils
 import fr.altzec.fr.altzec.icerunner.utils.TextGradientUtils
 import fr.altzec.fr.altzec.icerunner.world.WorldManager
 import net.md_5.bungee.api.ChatColor
@@ -16,6 +18,7 @@ import org.bukkit.command.TabExecutor
 import org.bukkit.plugin.PluginDescriptionFile
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.java.JavaPluginLoader
+import org.bukkit.util.CachedServerIcon
 import java.io.File
 
 open class Main : JavaPlugin {
@@ -27,6 +30,9 @@ open class Main : JavaPlugin {
     companion object {
         val GAME_NAME = "${ChatColor.GRAY}[${TextGradientUtils.generateGradient("IceRunner", "#59B1FD", "#59FDF8")}${ChatColor.GRAY}]${ChatColor.RESET}"
         val MAIN_PREFIX: String = "$GAME_NAME ${ChatColor.GRAY}\u00BB${ChatColor.RESET}"
+
+        private const val INTERNAL_GAME_ICON_PATH = "game_icon.png";
+        private const val EXTERNAL_GAME_ICON_PATH = "icon.png";
     }
 
     val pluginLogger = Bukkit.getLogger()
@@ -35,6 +41,8 @@ open class Main : JavaPlugin {
     val worldManager: WorldManager = WorldManager(this)
     val teamsManager: TeamsManager = TeamsManager(this)
     val scoreboardManager: ScoreboardManager = ScoreboardManager(this)
+
+    var serverCachedIcon: CachedServerIcon? = null;
 
     override fun onEnable() {
         super.onEnable()
@@ -50,6 +58,7 @@ open class Main : JavaPlugin {
         // Register event listeners
         listOf(
             PlayerJoinQuitListener(this),
+            ServerListPingListener(this),
             DevListener(this),
             ArrowListener(this),
             teamsManager,
@@ -58,6 +67,9 @@ open class Main : JavaPlugin {
         ).forEach { listener -> Bukkit.getPluginManager().registerEvents(listener, this) }
 
         Bukkit.getOnlinePlayers().forEach { player -> this.scoreboardManager.initPlayerScoreboard(player) }
+
+        // Server list related setup
+        prepareServerMotdAndIcon()
     }
 
     override fun onDisable() {
@@ -74,5 +86,18 @@ open class Main : JavaPlugin {
         command?.setExecutor(tabExecutor)
         command?.tabCompleter = tabExecutor
         pluginLogger.info("Command ${command?.usage} has been registered")
+    }
+
+    fun prepareServerMotdAndIcon() {
+        Bukkit.setMotd(
+            "               \u00bb ${ChatColor.YELLOW}Bienvenue en ${TextGradientUtils.generateGradient("IceRunner", "#59B1FD", "#59FDF8")}${ChatColor.YELLOW} !${ChatColor.GRAY} \u00ab\n" +
+                    "      ${ChatColor.YELLOW}Un ${ChatColor.GOLD}KOTH${ChatColor.YELLOW}, des ${TextGradientUtils.generateGradient("glissades", "#FFFFFF", "#59FDF8")}${ChatColor.YELLOW} et des ${TextGradientUtils.generateGradient("explosions", "ED524F", "EDA14F")} ${ChatColor.YELLOW}!"
+        )
+
+        val gameIconFile = File(dataFolder.path + File.separator + EXTERNAL_GAME_ICON_PATH);
+        if(!gameIconFile.exists()) {
+            FileUtils.copyResourceDir(INTERNAL_GAME_ICON_PATH, dataFolder.path + File.separator + EXTERNAL_GAME_ICON_PATH);
+        }
+        this.serverCachedIcon = Bukkit.loadServerIcon(File(dataFolder.path + File.separator + EXTERNAL_GAME_ICON_PATH))
     }
 }
