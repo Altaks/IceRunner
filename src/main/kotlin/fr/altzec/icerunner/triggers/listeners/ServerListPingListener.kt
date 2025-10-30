0af69ae230db17e7b1981a914a5c483858111a21
Altaks <altair61.dev@gmail.com>
2025-10-30 18:15:52 +0100

    Refactor/devtools (#22)
    * refactor: moved game world loading to the plugin enabling mode. Added support for world regeneration during reloads
    
    * feat: added back teleportation to center island when the player falls into the void during phases other than playing
    
    * feat: added motd and server icon
    
    * style: lint compliance

---
+++ src/main/kotlin/fr/altzec/icerunner/triggers/listeners/ServerListPingListener.kt
@@ -0,0 +1,16 @@
+package fr.altzec.fr.altzec.icerunner.triggers.listeners
+
+import fr.altzec.fr.altzec.icerunner.Main
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.server.ServerListPingEvent
+
+class ServerListPingListener(val main: Main) : Listener {
+
+    @EventHandler
+    fun onServerPingEvent(event: ServerListPingEvent) {
+        if (main.serverCachedIcon != null) {
+            event.setServerIcon(main.serverCachedIcon)
+        }
+    }
+}
